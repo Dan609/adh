ce6af55dd7dc192365f43efe08d98ac8668cf290
Dan Bobkov <39186942+Dan609@users.noreply.github.com>
2020-02-26 15:01:43 +0300

    .

--- adh.R
+++ adh.R
@@ -1,9 +1,10 @@
 # Immunofluorescence colocalozation analisys
 # Bobkov, Poloyanskaya, Goncharova, 2020
-# mailto: bobkov@incras.ru, 
+# mailto: bobkov@incras.ru
+# https://github.com/dan609/adh
 
 library(ggplot2)
-
+######################################
 #1# α-Actinin-4/F-actin colocalization 
 data_aA4_Fact_d <- read.csv('aA4_Fact_d.csv', sep = ';')
 
@@ -24,7 +25,7 @@ ggplot(data_aA4_Fact_d, aes(x = probe, y = bTau)) +
   
   theme(axis.text.x = element_text(angle = 0, hjust = 0)) + 
   
-  geom_hline(yintercept=0, linetype="dashed", color = "red") + # add zero
+  geom_hline(yintercept=0, linetype="dashed", color = "red") + # add zero level
   
   labs(title = "α-Actinin-4/F-actin",
        subtitle = "colocalization",
@@ -48,8 +49,7 @@ fit_aA4_Fact_d <- lm(bTau ~ probe, data = data_aA4_Fact_d)
 summary(fit_aA4_Fact_d)
 
 
-################################
-
+###########################################
 #2# α-Actinin-4/Hoechst33342 colocalozation
 data_aA4_Nuc_d <- read.csv('aA4_Nuc_d.csv', sep = ';')
 
@@ -68,22 +68,15 @@ ggplot(data_aA4_Nuc_d, aes(x = probe, y = bTau)) +
   
   theme(legend.position = "none") +
   
-  #  scale_x_discrete(labels= CellSciGuylabs) +
-  
-  labs(y = 'bTau',
-       x = "Cell Passage") +
-  
   theme(axis.text.x = element_text(angle = 0, hjust = 0)) + 
   
-  #  geom_boxplot(alpha = I(0.5), outlier.shape = NA, coef = 100) +
-  
-  geom_hline(yintercept=0, linetype="dashed", color = "red") +
+  geom_hline(yintercept=0, linetype="dashed", color = "red") + # add zero level
   
   labs(title = "α-Actinin-4/Hoechst33342",
        subtitle = "colocalization",
        caption = "Lm Coeff = 0.021\nR-squared = 0.29\np < 0.0001") +
   
-  geom_smooth(method='lm', formula= y ~ x) +
+  geom_smooth(method='lm', formula= y ~ x) + # add linear regression
   
   scale_x_continuous(name = "Cell Passage", 
                      breaks = c(1, seq(8, 17, 1)), 
@@ -94,19 +87,18 @@ ggplot(data_aA4_Nuc_d, aes(x = probe, y = bTau)) +
                      limits = c(-0.2, 0.4))
 
 
-# add linear regression
+# Compute linear regression
 
 fit-aA4_Nuc_d <- lm(bTau ~ probe, data = -aA4_Nuc_d)
 
 summary(fit-aA4_Nuc_d)
 
 
-################################
+###############################
+#3# RhoA/F-actin colocalization 
+data_rhoa-Fact-d <- read.csv('rhoa-Fact-d.csv', sep = ';')
 
-# 3 RhoA/F-actin
-data <- read.csv('rhoa-Fact-d.csv', sep = ';')
-
-ggplot(data, aes(x = probe, y = bTau)) +
+ggplot(data_rhoa-Fact-d, aes(x = probe, y = bTau)) +
   
   
   ylim(c(-.2, .4)) +
@@ -121,22 +113,15 @@ ggplot(data, aes(x = probe, y = bTau)) +
   
   theme(legend.position = "none") +
   
-  #  scale_x_discrete(labels= CellSciGuylabs) +
-  
-  labs(y = 'bTau',
-       x = "Cell Passage") +
-  
   theme(axis.text.x = element_text(angle = 0, hjust = 0)) + 
   
-  #  geom_boxplot(alpha = I(0.5), outlier.shape = NA, coef = 100) +
-  
-  geom_hline(yintercept=0, linetype="dashed", color = "red") +
+  geom_hline(yintercept=0, linetype="dashed", color = "red") + # add zero level
   
   labs(title = "RhoA/F-actin",
        subtitle = "colocalization",
        caption = "Lm Coeff = -0.02421; p =1.41e-12\n Data source: aA4_Nuc_d.csv") +
   
-  geom_smooth(method='lm', formula= y ~ x) +
+  geom_smooth(method='lm', formula= y ~ x) + # add linear regression
   
   scale_x_continuous(name = "Cell Passage", 
                      breaks = c(1, seq(8, 17, 1)), 
@@ -147,18 +132,17 @@ ggplot(data, aes(x = probe, y = bTau)) +
                      limits = c(-0.2, 0.4))
 
 
-# add linear regression
+# Compute linear regression
 
-fit3 <- lm(bTau ~ probe, data = data)
+fit_rhoa-Fact-d <- lm(bTau ~ probe, data = data_rhoa-Fact-d)
 
-summary(fit3)
+summary(fit_rhoa-Fact-d)
 
-################################
+###############################
+#4# RhoA/F-actin colocalization 
+data_rhoa-Nuc-d <- read.csv('rhoa-Nuc-d.csv', sep = ';')
 
-# 4 RhoA/F-actin
-data <- read.csv('rhoa-Nuc-d.csv', sep = ';')
-
-ggplot(data, aes(x = probe, y = bTau)) +
+ggplot(data_rhoa-Nuc-d, aes(x = probe, y = bTau)) +
   
   
   ylim(c(-.2, .4)) +
@@ -173,28 +157,21 @@ ggplot(data, aes(x = probe, y = bTau)) +
   
   theme(legend.position = "none") +
   
-  #  scale_x_discrete(labels= CellSciGuylabs) +
-  
-  labs(y = 'bTau',
-       x = "Cell Passage") +
-  
   theme(axis.text.x = element_text(angle = 0, hjust = 0)) + 
   
-  #  geom_boxplot(alpha = I(0.5), outlier.shape = NA, coef = 100) +
-  
-  geom_hline(yintercept=0, linetype="dashed", color = "red") +
+  geom_hline(yintercept=0, linetype="dashed", color = "red") + # add zero level
   
   labs(title = "RhoA/Hoechst33342",
        subtitle = "colocalization",
        caption = "Lm Coeff = 0.030896; p =3.82e-15\n Data source: aA4_Nuc_d.csv") +
   
-  geom_smooth(method='lm', formula= y ~ x)
+  geom_smooth(method='lm', formula= y ~ x) # add linear regression
 
 
-# add linear regression
+# Compute linear regression
 
-fit4 <- lm(bTau ~ probe, data = data)
+fit_rhoa-Nuc-d <- lm(bTau ~ probe, data = data_rhoa-Nuc-d)
 
-summary(fit4)
+summary(fit_rhoa-Nuc-d)
 
 
